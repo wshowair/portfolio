3246e98b2a231f3e5f8bff05aa9d30a9945ae584
Wael Showair <showair.wael@gmail.com>
2021-01-04 21:46:00 -0500

    update: pass server port number through env variable
    Closes #14

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,8 +8,8 @@ const greeting: Message = { message: 'Welcome to api!' }
 app.get('/api', (req, res) => {
   res.send(greeting)
 })
-
-const port = process.env.port || 3333
+const PORT_NUMBER = 3333
+const port = process.env.PORT_NUMBER || PORT_NUMBER
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port + '/api')
 })
