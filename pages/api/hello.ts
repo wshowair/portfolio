6b05344db89be82c055ccd8cbc46aa3b3617dc48
Wael Showair <showair.wael@gmail.com>
2022-11-06 02:56:51 -0500

    fix(Styles): no more styled components (#15)

--- pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type { User } from '../../api-interfaces/user'
-
-const getUser = (req: NextApiRequest, res: NextApiResponse<User>): void => {
-  res.status(200).json({ name: 'John Doe' })
-}
-
-export { getUser }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Main = styled.main`
-  background-color: ${(props) => props.theme.colors.primary};
-  width: 100%;
-`
 
 const Home = (): JSX.Element => {
-  return <Main id='main'>Homepage works</Main>
+  return <div id='main'>Hello World</div>
 }
 
 export default Home
